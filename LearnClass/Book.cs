2bd5267783891e8362b5748affb854650cc9a2ff
Sergei Vihlyancev <vihlyancevsy@gmail.com>
2017-12-13 23:08:37 +0400

    first program

---
+++ LearnClass/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnClass
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        public int year;
+
+        public Book()
+        {
+            name = "хуяк-хуяк и в продакшн";
+            author = "хуяк-хуяк и в продакшн";
+            year = 1;
+            /*
+             типо грибы, потому что в комментариях можно писать все, что угодно))
+             и еще немножко
+             и еще чуть-чуть
+            */
+        }
+        
+        public Book(string name, string author, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+        }
+
+        public void GetInformation()
+        {
+            Console.WriteLine($"Книга '{name}' (автор {author}) была издана в {year} году");
+        }
+    }
+}
---
+++ LearnClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Война и мир", "Л. Н. Толстой", 1869);
+            b1.GetInformation();
+            
+            Book b2 = new Book();
+            b2.GetInformation();
+            
+            Console.ReadLine();
+        }
+    }
+    
+}
